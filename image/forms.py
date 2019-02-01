5ffbe5522ed974062597b128a7ef9f78902512ab
Vinicius Monego <monego@noreply.github.com>
2019-02-01 17:08:01 -0200

    URLs de referência dos parâmetros (image)

--- image/forms.py
+++ image/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.utils.safestring import mark_safe
 
 # maximum size 25MB input file
 def file_size(value):
@@ -11,7 +12,7 @@ def file_size(value):
 
 class WaveletForm(forms.Form):
     Input = forms.ImageField(label="Image", required=True, validators=[file_size])
-    Wavelet = forms.ChoiceField(label="Wavelet", choices=(("db1", "db1"), ("db2", "db2"), ("db3", "db3")))
-    Method = forms.ChoiceField(label="Method", choices=(("visushrink", "VisuShrink"), ("bayeshrink", "BayesShrink")))
-    ShiftAmount = forms.IntegerField(label="Shift Amount", min_value=0, max_value=100)
+    Wavelet = forms.ChoiceField(label=mark_safe("<a href='http://scikit-image.org/docs/dev/api/skimage.restoration.html#denoise-wavelet'>Wavelet</a>"), choices=(("db1", "db1"), ("db2", "db2"), ("db3", "db3")))
+    Method = forms.ChoiceField(label=mark_safe("<a href='http://scikit-image.org/docs/dev/api/skimage.restoration.html#denoise-wavelet'>Method</a>"), choices=(("visushrink", "VisuShrink"), ("bayeshrink", "BayesShrink")))
+    ShiftAmount = forms.IntegerField(label=mark_safe("<a href='http://scikit-image.org/docs/dev/auto_examples/filters/plot_cycle_spinning.html'>Shift amount</a>"), min_value=0, max_value=100)
     Format = forms.ChoiceField(label="Format", choices=(("bmp", "BMP"), ("png", "PNG")))
